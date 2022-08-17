4e318482ee65be92cd452a5b2fdfb79be36eada3
Pietro <pietro.digiovanni.dev@gmail.com>
2022-08-17 12:36:13 +0200

    Enemy Spawner rework

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<GameObject> Enemies = new List<GameObject>();
+    [SerializeField] int nEnemiesToSpawn;
+    [SerializeField] float spawnDelay;
+    [field: SerializeField] public float remainingSpawnTime;
 
-    private void Start()
+    [SerializeField] Transform Player;
+    float Offset = 10f;
+
+    private void Update()
     {
-        StartCoroutine(SpawnEnemy(5f));        
-        StartCoroutine(SpawnEnemy(2f));        
-        StartCoroutine(SpawnEnemy(3f));        
-        StartCoroutine(SpawnEnemy(5f));
-        StartCoroutine(SpawnEnemy(6f));
+        remainingSpawnTime -= Time.deltaTime;
+
+        if (remainingSpawnTime <= 0f)
+        {
+            SpawnEnemy();
+        }
     }
 
-    IEnumerator SpawnEnemy(float spawnDelay)
+    void SpawnEnemy()
     {
-        while(true)
+        for (int i = 0; i < nEnemiesToSpawn; i++)
         {
-            yield return new WaitForSeconds(spawnDelay);
-            Instantiate(Enemies[Random.Range(0, Enemies.Count)], new Vector2(Random.Range(-20f, 20f), Random.Range(-20f, 20f)), Quaternion.identity);
+            Instantiate(Enemies[Random.Range(0, Enemies.Count)], new Vector2(
+                Random.Range(
+                    -(Player.position.x + Offset),
+                    Player.position.x + Offset
+                    ),
+                Random.Range(
+                    -(Player.position.y + Offset),
+                    Player.position.y + Offset
+                    )
+                ),
+                Quaternion.identity);
         }
+
+        remainingSpawnTime = spawnDelay;
     }
 }
